f04e249b1abd5e8d9cca92c78981532958413300
sasayabaku <sasayabaku@yahoo.co.jp>
2021-11-28 17:43:55 +0900

    Create Listup Image API

--- server/test/test_scraping.py
+++ server/test/test_scraping.py
@@ -4,8 +4,22 @@ sys.path.append('../src/')
 
 from scraping import listup_image
 
+import requests
+
+TARGET_URL = "https://www.irasutoya.com/"
 
 
 def test_listup():
-    assert len(listup_image("https://www.irasutoya.com/")) > 0
+    assert len(listup_image(TARGET_URL)) > 0
     assert listup_image("https://none.example.com/") == False
+
+def test_api_listup():
+    
+    response = requests.post(
+        "http://localhost:9999/listup",
+        json={
+            "url": TARGET_URL
+        }
+    )
+
+    assert len(response.json()['img_list']) > 0
