4d3bfd62f2dd4b189567451e8f60e9907c67422c
sasayabaku <sasayabaku@yahoo.co.jp>
2021-11-29 22:19:04 +0900

    Create Generate PDF API

--- server/test/test_scraping.py
+++ server/test/test_scraping.py
@@ -8,12 +8,14 @@ import requests
 
 TARGET_URL = "https://www.irasutoya.com/"
 
+_uuid = None
 
 def test_listup():
     assert len(listup_image(TARGET_URL)) > 0
     assert listup_image("https://none.example.com/") == False
 
 def test_api_listup():
+    global _uuid
     
     response = requests.post(
         "http://localhost:9999/listup",
@@ -22,4 +24,19 @@ def test_api_listup():
         }
     )
 
+    _uuid = response.json()['connection_id']
+
     assert len(response.json()['img_list']) > 0
+
+def test_api_generate_pdf():
+    global _uuid
+    
+    response = requests.post(
+        "http://localhost:9999/generate",
+        json={
+            "uuid": _uuid,
+            "indexes": [0, 1]
+        }
+    )
+
+    assert type(response.content) == bytes
